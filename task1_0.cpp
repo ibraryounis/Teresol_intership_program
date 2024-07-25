1544d73a976f7a4f0101ed0958bba4a464d6f55d
ibraryounis <ibraryounis027@gmail.com>
2024-07-25 15:17:06 +0500

    pratice

--- task1_0.cpp
+++ task1_0.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 int main()
 {
-    cout<<"Abrar Younis"<<endl;
+    cout<<"Enter the size of int="<<sizeof(int)<<"bytes"<<endl;
+    
+    cout<<"Enter the size of char="<<sizeof(char)<<"bytes"<<endl;
+     cout<<"Enter the size of float="<<sizeof(float)<<"bytes"<<endl;
+    cout<<"Enter the size of double="<<sizeof(double)<<"bytes"<<endl;
+    cout<<"Enter the size of bool="<<sizeof(bool)<<"bytes"<<endl;
+   
     return 0;
 }
